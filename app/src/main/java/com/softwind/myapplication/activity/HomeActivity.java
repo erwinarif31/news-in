1bccc52dcd747e82d1d2d8bf5309073db89d23a8
Sofyan Pujas <92896782+sofyanox12@users.noreply.github.com>
2023-04-08 10:51:54 +0800

    deal with the basics
    Add the directories of packages and a simple logo

--- app/src/main/java/com/softwind/myapplication/activity/HomeActivity.java
+++ app/src/main/java/com/softwind/myapplication/activity/HomeActivity.java
@@ -1,14 +1,16 @@
-package com.softwind.myapplication;
+package com.softwind.myapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.softwind.myapplication.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_activity);
     }
 }
