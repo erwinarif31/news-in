894a6ac0988befc96d014e2bf47d9de7c0d0f85e
erwinarif31 <muherwinarif31@gmail.com>
2023-04-08 14:49:44 +0800

    testing api to activity_home layout

--- app/src/main/java/com/softwind/myapplication/activity/HomeActivity.java
+++ app/src/main/java/com/softwind/myapplication/activity/HomeActivity.java
@@ -4,16 +4,26 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.softwind.myapplication.R;
+import com.softwind.myapplication.databinding.ActivityHomeBinding;
+import com.softwind.myapplication.models.Article;
 import com.softwind.myapplication.util.ApiClient;
 
 public class HomeActivity extends AppCompatActivity {
+    private ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_activity);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        ApiClient.getLatestNews(articles -> System.out.println(articles.length));
+        ApiClient.getLatestNews(articles -> setHeadline(articles[1]));
         ApiClient.getNewsWithCategory("science", articles -> System.out.println(articles[0].getImage_url()));
+    }
+
+    private void setHeadline(Article article) {
+        binding.tvTopHeadline.setText(article.getTitle());
+        Glide.with(this).load(article.getImage_url()).into(binding.topHeadlineImage);
     }
 }
