4d6449fb85be6994bc639ea5098f776cd5649124
Sofyan Pujas <92896782+sofyanox12@users.noreply.github.com>
2023-04-09 01:30:57 +0800

    add `RecyclerView` to `fragment_home.xml`
    Both for *Breaking News* and *For You News*

--- app/src/main/java/com/softwind/myapplication/fragment/HomeFragment.java
+++ app/src/main/java/com/softwind/myapplication/fragment/HomeFragment.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
-import com.softwind.myapplication.R;
 import com.softwind.myapplication.databinding.FragmentHomeBinding;
 import com.softwind.myapplication.models.Article;
 import com.softwind.myapplication.util.ApiClient;
@@ -24,7 +23,8 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ApiClient.getNewsWithCategory("science", articles -> setHeadline(articles[0]));
+        ApiClient.getNewsWithCategory("science", articles -> setHeadline(articles[1]));
+
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -39,7 +39,34 @@ public class HomeFragment extends Fragment {
     }
 
     private void setHeadline(Article article) {
+//        System.out.println("Title: " + article.getTitle());
+//        System.out.println("Image: " + article.getImage_url());
+//        System.out.println("Category: " + Arrays.toString(article.getCategory()));
+
         home.tvTopHeadline.setText(article.getTitle());
-        Glide.with(this).load(article.getImage_url()).into(home.topHeadlineImage);
+
+        if (!article.getImage_url().isEmpty()) {
+            Glide.with(this).load(article.getImage_url()).into(home.topHeadlineImage);
+
+        } else {
+//            String category = article.getCategory()[0];
+//            switch (category) {
+//                case "top":
+//                    break;
+//                case "business":
+//                    break;
+//                case "science":
+//                    break;
+//                case "technology":
+//                    break;
+//                case "sports":
+//                    break;
+//                case "health":
+//                    break;
+//                case "entertainment":
+//                    break;
+//            }
+        }
+
     }
 }
