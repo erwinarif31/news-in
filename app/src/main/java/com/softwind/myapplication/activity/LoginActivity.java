312582f8e8cd3f854efd262a99c9078f87b726b8
Sofyan Pujas <92896782+sofyanox12@users.noreply.github.com>
2023-04-23 00:34:55 +0800

    feature: authentication
    login & register with firebase Auth

--- app/src/main/java/com/softwind/myapplication/activity/LoginActivity.java
+++ app/src/main/java/com/softwind/myapplication/activity/LoginActivity.java
@@ -2,15 +2,82 @@ package com.softwind.myapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.softwind.myapplication.R;
+import com.softwind.myapplication.databinding.ActivityLoginBinding;
+import com.softwind.myapplication.fragment.ProfileFragment;
 
 public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        Boolean fromRegister = getIntent().getBooleanExtra("FROM_REGISTER", false);
+        if (mAuth.getCurrentUser() != null) openAuth(fromRegister);
+
+        setContentView(binding.getRoot());
+
+        binding.toSignUp.setOnClickListener(v -> {
+            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            finish();
+        });
+
+        binding.btnSignIn.setOnClickListener(v -> {
+            String email = binding.inputEmail.getText().toString();
+            String password = binding.inputPassword.getText().toString();
+            loginAuth(email, password);
+        });
+
+    }
+
+
+    private void loginAuth(String email, String password) {
+
+        if (email.isEmpty() || password.isEmpty()) {
+            binding.inputEmail.setError("Email is required");
+            binding.inputPassword.setError("Password is required");
+            return;
+        }
+
+        progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setMessage("Logging in...");
+        progressDialog.show();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+//                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                openAuth(true);
+
+            } else {
+                // If email is not found
+                if (task.getException().getMessage().equals("There is no user record corresponding to this identifier. The user may have been deleted.")) {
+                    binding.inputEmail.setError("Email does not exist!");
+                }
+                // If password is incorrect
+                if (task.getException().getMessage().equals("The password is invalid or the user does not have a password.")) {
+                    binding.inputPassword.setError("Password is incorrect!");
+                }
+            }
+
+            progressDialog.dismiss();
+        });
+
+    }
+
+    private void openAuth(Boolean toProfile) {
+        Intent toHome = new Intent(LoginActivity.this, MainActivity.class);
+        toHome.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        toHome.putExtra("TO_PROFILE", toProfile);
+        startActivity(toHome);
+        finish();
     }
 }
--- app/src/main/java/com/softwind/myapplication/activity/RegisterActivity.java
+++ app/src/main/java/com/softwind/myapplication/activity/RegisterActivity.java
@@ -2,15 +2,111 @@ package com.softwind.myapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.ktx.Firebase;
 import com.softwind.myapplication.R;
+import com.softwind.myapplication.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
+    private ActivityRegisterBinding binding;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        if (mAuth.getCurrentUser() != null) openAuth();
+        setContentView(binding.getRoot());
+
+        binding.toLogin.setOnClickListener(v -> {
+            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            finish();
+        });
+
+        binding.btnSignUp.setOnClickListener(v -> {
+            String fullName = binding.inputFullName.getText().toString();
+            String email = binding.inputEmail.getText().toString();
+            String password = binding.inputPassword.getText().toString();
+            String confirmPassword = binding.inputPasswordConfirmation.getText().toString();
+            registerAuth(fullName, email, password, confirmPassword);
+        });
+
+    }
+
+    private void registerAuth(String fullName, String email, String password, String confirmPassword) {
+
+        if (fullName.isEmpty()) {
+            binding.inputFullName.setError("Your Full name is required!");
+            return;
+
+        } else if (email.isEmpty()) {
+            binding.inputEmail.setError("Email is required!");
+            return;
+
+        } else if (password.isEmpty()) {
+            binding.inputPassword.setError("Password is required!");
+            return;
+
+        } else if (confirmPassword.isEmpty()) {
+            binding.inputPasswordConfirmation.setError("Confirm password is required!");
+            return;
+
+        } else if (!password.equals(confirmPassword)) {
+            binding.inputPassword.setError("Password doesn't match!");
+            binding.inputPasswordConfirmation.setError("Password doesn't match!");
+            return;
+        }
+
+        progressDialog = new ProgressDialog(RegisterActivity.this);
+        progressDialog.setMessage("Registering...");
+        progressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        UserProfileChangeRequest userData = new UserProfileChangeRequest
+                                .Builder()
+                                .setDisplayName(fullName)
+                                .build();
+
+                        assert user != null;
+                        user.updateProfile(userData).addOnCompleteListener(auth -> {
+                            if (auth.isSuccessful()) {
+//                                Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                                openLogin();
+                            }
+                        });
+
+                    } else
+                        binding.inputEmail.setError("We cannot fetch this one!");
+
+                    progressDialog.dismiss();
+                });
+    }
+
+    private void openAuth() {
+        Intent toHome = new Intent(RegisterActivity.this, MainActivity.class);
+        toHome.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        toHome.putExtra("TO_PROFILE", false);
+        startActivity(toHome);
+        finish();
+    }
+
+    private void openLogin() {
+        Intent toLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+        toLogin.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        toLogin.putExtra("FROM_REGISTER", true);
+        startActivity(toLogin);
+        finish();
     }
 }
